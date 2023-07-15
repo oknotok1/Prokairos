96179bb2447b1426d551b2cc6c27e17e41a589c7
Jeff <oknotok113@gmail.com>
2023-07-15 20:56:47 +0800

    Commiting homepage template

--- App.tsx
+++ App.tsx
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.body}>
       <StatusBar style="auto" />
-    </View>
+      <Header />
+      <Priorities />
+      <Timebox />
+    </SafeAreaView>
   );
 }
 
+const Header = () => {
+  const date = new Date();
+  const headerDate = date.toDateString();
+
+  return (
+    <View style={[styles.header, styles.container]}>
+      <Text style={styles.h1}>Prokairos</Text>
+      <Text style={styles.headerDate}>{headerDate}</Text>
+    </View>
+  );
+};
+
+const Priorities = () => {
+  return (
+    <View style={[styles.priorities, styles.container]}>
+      <Text style={styles.h2}>Top Priorities</Text>
+    </View>
+  );
+};
+
+const Timebox = () => {
+  return (
+    <View style={[styles.timebox, styles.container]}>
+      <Text style={styles.h2}>Timebox</Text>
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    gap: 16,
+    backgroundColor: "#fff",
+    // backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginHorizontal: 8,
   },
+  container: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    gap: 16,
+    backgroundColor: "lightgrey",
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+  },
+  header: {
+    marginTop: 16 * 2,
+  },
+  h1: {
+    fontSize: 16 * 3,
+  },
+  h2: {
+    fontSize: 16 * 2,
+  },
+  headerDate: {
+    fontSize: 16,
+  },
+  priorities: {},
+  timebox: {},
 });
